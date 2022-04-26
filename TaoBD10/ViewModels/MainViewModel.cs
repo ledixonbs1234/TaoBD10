f7e938e79ea29f095dc929e4e615fe9eeff0fdea
le di xon <ledixonbs1234@hotmail.com>
2022-04-26 20:54:38 +0700

    last

--- TaoBD10/ViewModels/MainViewModel.cs
+++ TaoBD10/ViewModels/MainViewModel.cs
@@ -130,7 +130,7 @@ namespace TaoBD10.ViewModels
                 return;
             var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
             _window.Width = 360;
-            _window.Height = 670;
+            _window.Height = 640;
             double height = System.Windows.SystemParameters.PrimaryScreenHeight;
             double width = System.Windows.SystemParameters.PrimaryScreenWidth;
             // use 'Screen.AllScreens[1].WorkingArea' for secondary screen

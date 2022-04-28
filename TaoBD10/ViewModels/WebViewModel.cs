869310b9d795766fbf806bffc9aea1fd8fbcfeaf
ledixonbs1234 <ledixonbs12345@gmail.com>
2022-04-28 22:53:37 +0700

    dfdf

---
+++ TaoBD10/ViewModels/WebViewModel.cs
@@ -0,0 +1,51 @@
+﻿using CefSharp.Wpf;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TaoBD10.ViewModels
+{
+    public class WebViewModel :ObservableObject
+    {
+        public IRelayCommand<ChromiumWebBrowser> LoadPageCommand;
+
+        private ChromiumWebBrowser _WebBrowser;
+
+        public ChromiumWebBrowser WebBrowser
+        {
+            get { return _WebBrowser; }
+            set { SetProperty(ref _WebBrowser, value); }
+        }
+
+        public WebViewModel()
+        {
+            LoadPageCommand = new RelayCommand<ChromiumWebBrowser>(LoadPage);
+            LoginCommand = new RelayCommand(Login);
+        }
+
+        void Login()
+        {
+
+        }
+
+        private string _AddressWeb = "https://www.google.com";
+
+        public string AddressWeb
+        {
+            get { return _AddressWeb; }
+            set {SetProperty(ref _AddressWeb , value); }
+        }
+
+        public ICommand LoginCommand;
+
+
+        void LoadPage(ChromiumWebBrowser web)
+        {
+        }
+    }
+}

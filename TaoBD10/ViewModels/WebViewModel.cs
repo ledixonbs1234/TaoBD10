e3a8d54150b56087f510106b770ada35843a96ac
le di xon <ledixonbs1234@hotmail.com>
2022-04-29 23:12:05 +0700

    ss

--- TaoBD10/ViewModels/WebViewModel.cs
+++ TaoBD10/ViewModels/WebViewModel.cs
@@ -42,7 +42,60 @@ namespace TaoBD10.ViewModels
         public string AddressWeb
         {
             get { return _AddressWeb; }
-            set {SetProperty(ref _AddressWeb , value); }
+            set {SetProperty(ref _AddressWeb , value);
+                if (!isInitializeWeb)
+                {
+                    WebBrowser.LoadingStateChanged += WebBrowser_LoadingStateChanged;
+                }
+            }
+        }
+
+        private void WebBrowser_LoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
+        {
+            if (!e.IsLoading)
+            {
+                //Da tai xong
+                string diachi = AddressWeb.ToLower();
+
+                if (diachi.IndexOf("login") != -1)
+                {
+                    string scriptFirst = @"
+Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+}
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+    for(var i = this.length - 1; i >= 0; i--) {
+        if(this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+}
+document.getElementById(""vnpNav"").remove();
+document.getElementsByClassName("".mainHomepageTitle"").remove();
+document.getElementsByClassName("".mainHomepageUserLogin"").remove();
+document.getElementsByClassName("".footer"").remove();
+
+                                    var textbox = document.getElementById(""MainContent_imgCaptcha"");
+    textbox.focus();
+                    textbox.scrollIntoView();
+                    ";
+                    WebBrowser.ExecuteScriptAsync(scriptFirst);
+                    //string html= await chrWeb.GetSourceAsync();
+                    //Regex regexLogin = new Regex(@"MainContent_imgCaptcha"" src=""((\w|\W)+?)""");
+                    //var matchLogin = regexLogin.Match(html);
+                    //if (matchLogin != null)
+                    //{
+
+                    //    picImage.Load("https://bccp.vnpost.vn/" + matchLogin.Groups[1].Value);
+                    //}
+
+                }
+                else
+                {
+                    //kiem tra dieu kien url
+
+                }
+            }
         }
 
         public ICommand LoginCommand;

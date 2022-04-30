a3b297ac386ec38a0842dc1058ec6121adfb1d5d
ledixonbs1234 <ledixonbs12345@gmail.com>
2022-04-30 18:09:05 +0700

    sdsd

---
+++ TaoBD10/Model/WebContentModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaoBD10.Model
+{
+    public class WebContentModel
+    {
+        public string Code { get; set; }
+        public string AddressSend { get; set; }
+        public string AddressReiceive { get; set; }
+        public string BuuCucPhat { get; set; }
+        public string BuuCucGui { get; set; }
+    }
+}

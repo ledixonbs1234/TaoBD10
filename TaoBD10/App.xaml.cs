8f134e7340e0574326d32dae4ec38104f247e9bc
ledixonbs1234 <ledixonbs12345@gmail.com>
2022-05-04 18:43:34 +0700

    asdsa

--- TaoBD10/App.xaml.cs
+++ TaoBD10/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace TaoBD10
 {
@@ -9,6 +11,26 @@ namespace TaoBD10
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+            Current.DispatcherUnhandledException += new DispatcherUnhandledExceptionEventHandler(Current_DispatcherUnhandledException);
+            DispatcherUnhandledException += new DispatcherUnhandledExceptionEventHandler(App_DispatcherUnhandledException);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+        }
+
+        void Current_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            // we cannot handle this, but not to worry, I have not encountered this exception yet.  
+            // However, you can show/log the exception message and show a message that if the application is terminating or not.  
+            var isTerminating = e.IsTerminating;
         }
     }
 }

175b5628620186962b590ee2c6d2bb47de5d6c17
le di xon <ledixonbs1234@hotmail.com>
2022-05-19 13:11:32 +0700

    cap nhat lay chuyen thu

--- TaoBD10/ViewModels/LayChuyenThuViewModel.cs
+++ TaoBD10/ViewModels/LayChuyenThuViewModel.cs
@@ -1,24 +1,27 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
 using Microsoft.Toolkit.Mvvm.Input;
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using System.Threading;
 using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Threading;
 using TaoBD10.Manager;
+using TaoBD10.Model;
 using static TaoBD10.Manager.EnumAll;
 
 namespace TaoBD10.ViewModels
 {
     public class LayChuyenThuViewModel : ObservableObject
     {
+
+        BackgroundWorker bwLayCT;
         public LayChuyenThuViewModel()
         {
-            timer = new DispatcherTimer
-            {
-                Interval = new TimeSpan(2000)
-            };
-            timer.Tick += Timer_Tick;
+            bwLayCT = new BackgroundWorker();
+            bwLayCT.DoWork += BwLayCT_DoWork;
             BongSonCommand = new RelayCommand(BongSon);
             HoaiXuanCommand = new RelayCommand(HoaiXuan);
             HoaiTanCommand = new RelayCommand(HoaiTan);
@@ -28,6 +31,45 @@ namespace TaoBD10.ViewModels
             HoaiHaiCommand = new RelayCommand(HoaiHai);
             BCPCommand = new RelayCommand(BCP);
             HoaiMyCommand = new RelayCommand(HoaiMy);
+        }
+
+        private void BwLayCT_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            WindowInfo window = APIManager.WaitingFindedWindow("quan ly chuyen thu");
+            if (window == null)
+            {
+                APIManager.ShowSnackbar("Không tìm thấy quản lý chuyến thư");
+                return;
+            }
+
+            List<TestAPIModel> controls = APIManager.GetListControlText(window.hwnd);
+            if (controls.Count == 0)
+            {
+                APIManager.ShowSnackbar("Window lỗi");
+                return;
+            }
+            List<TestAPIModel> listCombobox = controls.Where(m => m.ClassName.ToLower().IndexOf("combobox") != -1).ToList();
+            IntPtr comboHandle = listCombobox[3].Handle;
+
+            APIManager.SendMessage(comboHandle, 0x0007, 0, 0);
+            APIManager.SendMessage(comboHandle, 0x0007, 0, 0);
+
+            ChuyenThuDen();
+            Thread.Sleep(200);
+            SendKeys.SendWait("{ENTER}");
+
+            window = APIManager.WaitingFindedWindow("canh bao", "thong bao");
+            if (window == null)
+            {
+                APIManager.ShowSnackbar("Không tìm thấy cảnh báo");
+                return;
+            }
+
+            SendKeys.SendWait("{ENTER}");
+            Thread.Sleep(500);
+            SendKeys.SendWait("{F5}");
+
         }
 
         public ICommand BCPCommand { get; }
@@ -44,7 +86,6 @@ namespace TaoBD10.ViewModels
         {
             maBuuCucChuyenThuDen = "593200";
             isBaoDamChuyenThuDen = false;
-            stateChuyenThuChieuDen = StateChuyenThuChieuDen.GetData;
 
             if (!APIManager.ThoatToDefault("593230", "quan ly chuyen thu chieu den"))
             {
@@ -52,13 +93,12 @@ namespace TaoBD10.ViewModels
                 Thread.Sleep(200);
                 SendKeys.SendWait("3");
             }
-            timer.Start();
+            bwLayCT.RunWorkerAsync();
         }
 
         private void BCP()
         {
             maBuuCucChuyenThuDen = "593280";
-            stateChuyenThuChieuDen = StateChuyenThuChieuDen.GetData;
             isBaoDamChuyenThuDen = false;
             if (!APIManager.ThoatToDefault("593230", "quan ly chuyen thu chieu den"))
             {
@@ -66,13 +106,12 @@ namespace TaoBD10.ViewModels
                 Thread.Sleep(200);
                 SendKeys.SendWait("3");
             }
-            timer.Start();
+            bwLayCT.RunWorkerAsync();
         }
 
         private void BongSon()
         {
             maBuuCucChuyenThuDen = "593522";
-            stateChuyenThuChieuDen = StateChuyenThuChieuDen.GetData;
             isBaoDamChuyenThuDen = true;
             if (!APIManager.ThoatToDefault("593230", "quan ly chuyen thu chieu den"))
             {
@@ -80,12 +119,12 @@ namespace TaoBD10.ViewModels
                 Thread.Sleep(200);
                 SendKeys.SendWait("3");
             }
-            timer.Start();
+            bwLayCT.RunWorkerAsync();
         }
 
-        private void ChuyenThuDen(bool isBaoDam, string mabuucuc)
+        private void ChuyenThuDen()
         {
-            if (isBaoDam)
+            if (isBaoDamChuyenThuDen)
             {
                 SendKeys.SendWait("{UP}{UP}{UP}{UP}{UP}{UP}{UP}{UP}{UP}");
                 //buu pham bao dam
@@ -98,7 +137,7 @@ namespace TaoBD10.ViewModels
                 SendKeys.SendWait("{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}");
             }
             SendKeys.SendWait("{TAB}");
-            SendKeys.SendWait(mabuucuc);
+            SendKeys.SendWait(maBuuCucChuyenThuDen);
             SendKeys.SendWait("{TAB}");
             SendKeys.SendWait("{F8}");
         }
@@ -106,7 +145,6 @@ namespace TaoBD10.ViewModels
         private void GiaoDich()
         {
             maBuuCucChuyenThuDen = "593200";
-            stateChuyenThuChieuDen = StateChuyenThuChieuDen.GetData;
             isBaoDamChuyenThuDen = false;
             if (!APIManager.ThoatToDefault("593230", "quan ly chuyen thu chieu den"))
             {
@@ -114,13 +152,12 @@ namespace TaoBD10.ViewModels
                 Thread.Sleep(200);
                 SendKeys.SendWait("3");
             }
-            timer.Start();
+            bwLayCT.RunWorkerAsync();
         }
 
         private void HoaiDuc()
         {
             maBuuCucChuyenThuDen = "593550";
-            stateChuyenThuChieuDen = StateChuyenThuChieuDen.GetData;
             isBaoDamChuyenThuDen = true;
             if (!APIManager.ThoatToDefault("593230", "quan ly chuyen thu chieu den"))
             {
@@ -128,13 +165,12 @@ namespace TaoBD10.ViewModels
                 Thread.Sleep(200);
                 SendKeys.SendWait("3");
             }
-            timer.Start();
+            bwLayCT.RunWorkerAsync();
         }
 
         private void HoaiHai()
         {
             maBuuCucChuyenThuDen = "593260";
-            stateChuyenThuChieuDen = StateChuyenThuChieuDen.GetData;
             isBaoDamChuyenThuDen = true;
             if (!APIManager.ThoatToDefault("593230", "quan ly chuyen thu chieu den"))
             {
@@ -142,13 +178,12 @@ namespace TaoBD10.ViewModels
                 Thread.Sleep(200);
                 SendKeys.SendWait("3");
             }
-            timer.Start();
+            bwLayCT.RunWorkerAsync();
         }
 
         private void HoaiHuong()
         {
             maBuuCucChuyenThuDen = "593270";
-            stateChuyenThuChieuDen = StateChuyenThuChieuDen.GetData;
             isBaoDamChuyenThuDen = true;
             if (!APIManager.ThoatToDefault("593230", "quan ly chuyen thu chieu den"))
             {
@@ -156,13 +191,12 @@ namespace TaoBD10.ViewModels
                 Thread.Sleep(200);
                 SendKeys.SendWait("3");
             }
-            timer.Start();
+            bwLayCT.RunWorkerAsync();
         }
 
         private void HoaiMy()
         {
             maBuuCucChuyenThuDen = "593240";
-            stateChuyenThuChieuDen = StateChuyenThuChieuDen.GetData;
             isBaoDamChuyenThuDen = true;
             if (!APIManager.ThoatToDefault("593230", "quan ly chuyen thu chieu den"))
             {
@@ -170,13 +204,12 @@ namespace TaoBD10.ViewModels
                 Thread.Sleep(200);
                 SendKeys.SendWait("3");
             }
-            timer.Start();
+            bwLayCT.RunWorkerAsync();
         }
 
         private void HoaiTan()
         {
             maBuuCucChuyenThuDen = "593370";
-            stateChuyenThuChieuDen = StateChuyenThuChieuDen.GetData;
             isBaoDamChuyenThuDen = true;
             if (!APIManager.ThoatToDefault("593230", "quan ly chuyen thu chieu den"))
             {
@@ -184,13 +217,12 @@ namespace TaoBD10.ViewModels
                 Thread.Sleep(200);
                 SendKeys.SendWait("3");
             }
-            timer.Start();
+            bwLayCT.RunWorkerAsync();
         }
 
         private void HoaiXuan()
         {
             maBuuCucChuyenThuDen = "593220";
-            stateChuyenThuChieuDen = StateChuyenThuChieuDen.GetData;
             isBaoDamChuyenThuDen = true;
             if (!APIManager.ThoatToDefault("593230", "quan ly chuyen thu chieu den"))
             {
@@ -198,96 +230,10 @@ namespace TaoBD10.ViewModels
                 Thread.Sleep(200);
                 SendKeys.SendWait("3");
             }
-            timer.Start();
+            bwLayCT.RunWorkerAsync();
         }
-
-        private void Timer_Tick(object sender, EventArgs e)
-        {
-            if (isWaitingChuyenThuChieuDen)
-                return;
-            var currentWindow = APIManager.GetActiveWindowTitle();
-            if (currentWindow == null)
-                return;
-            APIManager.ShowTest("1");
-
-            switch (stateChuyenThuChieuDen)
-            {
-                case StateChuyenThuChieuDen.GetData:
-                    if (currentWindow.text.IndexOf("quan ly chuyen thu") != -1)
-                    {
-                        Thread.Sleep(200);
-                        isWaitingChuyenThuChieuDen = true;
-
-                        //lay combobox hien tai
-                        var childHandles3 = APIManager.GetAllChildHandles(currentWindow.hwnd);
-                        int countCombobox = 0;
-                        IntPtr combo = IntPtr.Zero;
-                        foreach (var item in childHandles3)
-                        {
-                            string className = APIManager.GetWindowClass(item);
-                            string classDefault = "WindowsForms10.COMBOBOX.app.0.1e6fa8e";
-                            //string classDefault = "WindowsForms10.COMBOBOX.app.0.141b42a_r8_ad1";
-                            if (className == classDefault)
-                            {
-                                if (countCombobox == 3)
-                                {
-                                    //road = item;
-                                    combo = item;
-                                    break;
-                                }
-                                countCombobox++;
-                            }
-                        }
-                        APIManager.ShowTest("2");
-                        APIManager.SendMessage(combo, 0x0007, 0, 0);
-                        APIManager.SendMessage(combo, 0x0007, 0, 0);
-
-                        ChuyenThuDen(isBaoDamChuyenThuDen, maBuuCucChuyenThuDen);
-                        stateChuyenThuChieuDen = StateChuyenThuChieuDen.ShowInfo;
-                        Thread.Sleep(200);
-                        SendKeys.SendWait("{ENTER}");
-
-                        isWaitingChuyenThuChieuDen = false;
-                    }
-
-                    break;
-
-                case StateChuyenThuChieuDen.ShowInfo:
-                    APIManager.ShowTest("3");
-                    if (currentWindow.text.IndexOf("canh bao") != -1)
-                    {
-                        isWaitingChuyenThuChieuDen = true;
-                        SendKeys.SendWait("{ENTER}");
-                        Thread.Sleep(500);
-                        SendKeys.SendWait("{F5}");
-                        timer.Stop();
-                        isWaitingChuyenThuChieuDen = false;
-                    }
-                    else if (currentWindow.text.IndexOf("thong bao") != -1)
-                    {
-                        isWaitingChuyenThuChieuDen = true;
-                        SendKeys.SendWait("{ENTER}");
-                        Thread.Sleep(500);
-                        SendKeys.SendWait("{F5}");
-                        timer.Stop();
-                        isWaitingChuyenThuChieuDen = false;
-                    }
-                    APIManager.ShowTest("4");
-
-                    break;
-
-                case StateChuyenThuChieuDen.Find:
-                    break;
-
-                default:
-                    break;
-            }
-        }
-
         private bool isBaoDamChuyenThuDen = false;
         private bool isWaitingChuyenThuChieuDen = false;
         private string maBuuCucChuyenThuDen = "";
-        private StateChuyenThuChieuDen stateChuyenThuChieuDen = StateChuyenThuChieuDen.GetData;
-        private readonly DispatcherTimer timer;
     }
 }

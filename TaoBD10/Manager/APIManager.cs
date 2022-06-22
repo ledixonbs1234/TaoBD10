2ebc2ee6d1f8f6a1853442b879d10ac514e1a977
le di xon <ledixonbs1234@hotmail.com>
2022-06-22 22:45:08 +0700

    update

--- TaoBD10/Manager/APIManager.cs
+++ TaoBD10/Manager/APIManager.cs
@@ -15,49 +15,24 @@ namespace TaoBD10.Manager
 {
     public static class APIManager
     {
-        public delegate bool EnumWindowProc(IntPtr hwnd, IntPtr lParam);
-        public static string namePrinterBD8 = "";
-        public static string namePrinterBD10 = "";
-
-        public static string ConvertToUnSign3(string s)
-        {
-            Regex regex = new Regex("\\p{IsCombiningDiacriticalMarks}+");
-            string temp = s.Normalize(NormalizationForm.FormD);
-            return regex.Replace(temp, String.Empty).Replace('\u0111', 'd').Replace('\u0110', 'D').ToLower();
-        }
-        public static int GetLineNumber(Exception ex)
-        {
-            var lineNumber = 0;
-            const string lineSearch = ":line ";
-            var index = ex.StackTrace.LastIndexOf(lineSearch);
-            if (index != -1)
-            {
-                var lineNumberText = ex.StackTrace.Substring(index + lineSearch.Length);
-                if (int.TryParse(lineNumberText, out lineNumber))
-                {
-                }
-            }
-            return lineNumber;
-        }
-
         public static void ClearClipboard()
         {
             Thread thread = new Thread(() => System.Windows.Clipboard.Clear());
         }
-        //private static int WM_LBUTTONDOWN = 0x0201;
-        //private static int WM_LBUTTONUP = 0x0202;
 
         public static void ClickButton(IntPtr handle)
         {
             PostMessage(handle, 0x00F5, 0, 0);
         }
 
-        public static void ClickButton(IntPtr window,string name)
+        //private static int WM_LBUTTONDOWN = 0x0201;
+        //private static int WM_LBUTTONUP = 0x0202;
+        public static void ClickButton(IntPtr window, string name)
         {
             List<TestAPIModel> controls = APIManager.GetListControlText(window);
             foreach (TestAPIModel control in controls)
             {
-                if(control.Text == name)
+                if (control.Text == name)
                 {
                     ClickButton(control.Handle);
                     break;
@@ -66,27 +41,18 @@ namespace TaoBD10.Manager
 
         }
 
-        [DllImport("user32.dll")]
-        static extern bool PostMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
-        public static string GetCopyData()
+        public static void ClickButton(IntPtr mainHandle, IntPtr buttonHandle)
         {
-            string clipboard = "";
-            Thread thread;
-            thread = new Thread(() => clipboard = System.Windows.Clipboard.GetText());
-            thread.SetApartmentState(ApartmentState.STA); //Set the thread to STA
-            for (int i = 0; i < 10; i++)
-            {
-                SendKeys.SendWait("^(c)");
-                Thread.Sleep(50);
 
-                thread.Start();
-                thread.Join(); //Wait for the thread to end
-                if (!string.IsNullOrEmpty(clipboard))
-                {
-                    break;
-                }
-            }
-            return clipboard;
+            int wparam = (0 << 16) | (0x79 & 0xffff);
+            APIManager.SendMessage(mainHandle, WM_COMMAND, wparam, (int)buttonHandle);
+        }
+
+        public static string ConvertToUnSign3(string s)
+        {
+            Regex regex = new Regex("\\p{IsCombiningDiacriticalMarks}+");
+            string temp = s.Normalize(NormalizationForm.FormD);
+            return regex.Replace(temp, String.Empty).Replace('\u0111', 'd').Replace('\u0110', 'D').ToLower();
         }
 
         public static bool EnumWindow(IntPtr hWnd, IntPtr lParam)
@@ -102,143 +68,6 @@ namespace TaoBD10.Manager
             childHandles.Add(hWnd);
 
             return true;
-        }
-
-        public static void ShowTest(string content)
-        {
-            WeakReferenceMessenger.Default.Send<ContentModel>(new ContentModel { Key = "Test", Content = content });
-        }
-        [DllImport("user32.dll", EntryPoint = "SetWindowText")]
-        private static extern int SetWindowText(IntPtr hWnd, string text);
-        [DllImport("user32.dll", EntryPoint = "FindWindowEx")]
-        private static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
-        [DllImport("User32.dll", EntryPoint = "SendMessage")]
-        private static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, string lParam);
-
-        public static void OpenNotePad(string message = null, string title = null)
-        {
-            Process notepad = Process.Start(new ProcessStartInfo("notepad.exe"));
-            if (notepad != null)
-            {
-                notepad.WaitForInputIdle();
-
-                if (!string.IsNullOrEmpty(title))
-                    SetWindowText(notepad.MainWindowHandle, title);
-
-                if (!string.IsNullOrEmpty(message))
-                {
-                    IntPtr child = FindWindowEx(notepad.MainWindowHandle, new IntPtr(0), "Edit", null);
-                    SendMessage(child, 0x000C, 0, message);
-                }
-            }
-        }
-
-        public static void ShowSnackbar(string content)
-        {
-            WeakReferenceMessenger.Default.Send<ContentModel>(new ContentModel { Key = "Snackbar", Content = content });
-        }
-
-        [DllImport("winspool.drv", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern bool SetDefaultPrinter(string Name);
-
-        public static void SetPrintBD8()
-        {
-            if (!string.IsNullOrEmpty(namePrinterBD8))
-            {
-                SetDefaultPrinter(namePrinterBD8);
-            }
-            
-        }
-        public static void SetPrintBD10()
-        {
-            if (!string.IsNullOrEmpty(namePrinterBD10))
-            {
-                SetDefaultPrinter(namePrinterBD10);
-            }
-        }
-
-        /// <summary>
-        /// Cho toi khi nao co active window
-        /// </summary>
-        /// <param name="title"> tieu de</param>
-        /// <param name="time">thoi gian s* 0.2</param>
-        /// <param name="isExactly">co chinh xac title khong</param>
-        /// <returns>null neu khong tim thay</returns>
-        public static WindowInfo WaitingFindedWindow(string title, string title2 = "null", int time = 8, bool isExactly = false)
-        {
-            try
-            {
-                WindowInfo currentWindow = null;
-                string titleWindow = "";
-                time *= 5;
-                if (isExactly)
-                {
-                    while (titleWindow != title)
-                    {
-                        time--;
-                        if (time <= 0)
-                            return null;
-
-                        Thread.Sleep(200);
-                        currentWindow = GetActiveWindowTitle(true);
-
-                        titleWindow = currentWindow.text;
-                    }
-                }
-                else
-                {
-                    while (titleWindow.IndexOf(title) == -1 && titleWindow.IndexOf(title2) == -1)
-                    {
-                        time--;
-                        if (time <= 0)
-                            return null;
-
-                        Thread.Sleep(200);
-                        currentWindow = GetActiveWindowTitle();
-                        if (!string.IsNullOrEmpty(currentWindow.text))
-                        {
-                            titleWindow = ConvertToUnSign3(currentWindow.text).ToLower();
-                        }
-
-                    }
-                }
-                Thread.Sleep(100);
-                return currentWindow;
-            }
-            catch (Exception ex)
-            {
-                // Get stack trace for the exception with source file information
-                var st = new StackTrace(ex, true);
-                // Get the top stack frame
-                var frame = st.GetFrame(0);
-                // Get the line number from the stack frame
-                var line = frame.GetFileLineNumber();
-                APIManager.OpenNotePad(ex.Message + '\n' + "loi Line WebViewModel " + line + " Number Line " + APIManager.GetLineNumber(ex), "loi ");
-                throw;
-            }
-
-        }
-
-        public static List<TestAPIModel> GetListControlText(IntPtr handleActiveWindow)
-        {
-            var allChild = GetAllChildHandles(handleActiveWindow);
-
-            List<TestAPIModel> list = new List<TestAPIModel>();
-            int count = 0;
-            foreach (var item in allChild)
-            {
-
-                TestAPIModel test = new TestAPIModel
-                {
-                    Index = count,
-                    Text = GetControlText(item),
-                    Handle = item,
-                    ClassName = GetWindowClass(item)
-                };
-                list.Add(test);
-                count++;
-            }
-            return list;
         }
 
         public static WindowInfo GetActiveWindowTitle(bool isExactly = false)
@@ -298,11 +127,69 @@ namespace TaoBD10.Manager
             return title.ToString();
         }
 
+        public static string GetCopyData()
+        {
+            string clipboard = "";
+            Thread thread;
+            thread = new Thread(() => clipboard = System.Windows.Clipboard.GetText());
+            thread.SetApartmentState(ApartmentState.STA); //Set the thread to STA
+            for (int i = 0; i < 10; i++)
+            {
+                SendKeys.SendWait("^(c)");
+                Thread.Sleep(50);
+
+                thread.Start();
+                thread.Join(); //Wait for the thread to end
+                if (!string.IsNullOrEmpty(clipboard))
+                {
+                    break;
+                }
+            }
+            return clipboard;
+        }
+
         [DllImport("user32.dll")]
         public static extern IntPtr GetForegroundWindow();
 
         [DllImport("user32.dll")]
         public static extern IntPtr GetLastActivePopup(IntPtr hWnd);
+
+        public static int GetLineNumber(Exception ex)
+        {
+            var lineNumber = 0;
+            const string lineSearch = ":line ";
+            var index = ex.StackTrace.LastIndexOf(lineSearch);
+            if (index != -1)
+            {
+                var lineNumberText = ex.StackTrace.Substring(index + lineSearch.Length);
+                if (int.TryParse(lineNumberText, out lineNumber))
+                {
+                }
+            }
+            return lineNumber;
+        }
+
+        public static List<TestAPIModel> GetListControlText(IntPtr handleActiveWindow)
+        {
+            var allChild = GetAllChildHandles(handleActiveWindow);
+
+            List<TestAPIModel> list = new List<TestAPIModel>();
+            int count = 0;
+            foreach (var item in allChild)
+            {
+
+                TestAPIModel test = new TestAPIModel
+                {
+                    Index = count,
+                    Text = GetControlText(item),
+                    Handle = item,
+                    ClassName = GetWindowClass(item)
+                };
+                list.Add(test);
+                count++;
+            }
+            return list;
+        }
 
         public static string GetWindowClass(IntPtr hWnd)
         {
@@ -331,22 +218,61 @@ namespace TaoBD10.Manager
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
 
+        public static void OpenNotePad(string message = null, string title = null)
+        {
+            Process notepad = Process.Start(new ProcessStartInfo("notepad.exe"));
+            if (notepad != null)
+            {
+                notepad.WaitForInputIdle();
+
+                if (!string.IsNullOrEmpty(title))
+                    SetWindowText(notepad.MainWindowHandle, title);
+
+                if (!string.IsNullOrEmpty(message))
+                {
+                    IntPtr child = FindWindowEx(notepad.MainWindowHandle, new IntPtr(0), "Edit", null);
+                    SendMessage(child, 0x000C, 0, message);
+                }
+            }
+        }
+
         [System.Runtime.InteropServices.DllImport("user32.dll")]
         public static extern IntPtr SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
         public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, StringBuilder lParam);
+
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
-        public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, int wParam,IntPtr lParam);
+        public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, int wParam, IntPtr lParam);
 
         [DllImportAttribute("User32.dll")]
         public static extern IntPtr SetForegroundWindow(IntPtr hWnd);
-        const int WM_COMMAND = 0x0111;
-        public static void ClickButton(IntPtr mainHandle,IntPtr buttonHandle)
-        {
 
-            int wparam = (0 << 16) | (0x79 & 0xffff);
-            APIManager.SendMessage(mainHandle, WM_COMMAND, wparam, (int)buttonHandle);
+        public static void SetPrintBD10()
+        {
+            if (!string.IsNullOrEmpty(namePrinterBD10))
+            {
+                SetDefaultPrinter(namePrinterBD10);
+            }
+        }
+
+        public static void SetPrintBD8()
+        {
+            if (!string.IsNullOrEmpty(namePrinterBD8))
+            {
+                SetDefaultPrinter(namePrinterBD8);
+            }
+
+        }
+
+        public static void ShowSnackbar(string content)
+        {
+            WeakReferenceMessenger.Default.Send<ContentModel>(new ContentModel { Key = "Snackbar", Content = content });
+        }
+
+        public static void ShowTest(string content)
+        {
+            WeakReferenceMessenger.Default.Send<ContentModel>(new ContentModel { Key = "Test", Content = content });
         }
 
         /// <summary>
@@ -441,11 +367,92 @@ namespace TaoBD10.Manager
 
         }
 
+        /// <summary>
+        /// Cho toi khi nao co active window
+        /// </summary>
+        /// <param name="title"> tieu de</param>
+        /// <param name="time">thoi gian s* 0.2</param>
+        /// <param name="isExactly">co chinh xac title khong</param>
+        /// <returns>null neu khong tim thay</returns>
+        public static WindowInfo WaitingFindedWindow(string title, string title2 = "null", int time = 8, bool isExactly = false)
+        {
+            try
+            {
+                WindowInfo currentWindow = null;
+                string titleWindow = "";
+                time *= 5;
+                if (isExactly)
+                {
+                    while (titleWindow != title)
+                    {
+                        time--;
+                        if (time <= 0)
+                            return null;
+
+                        Thread.Sleep(200);
+                        currentWindow = GetActiveWindowTitle(true);
+
+                        titleWindow = currentWindow.text;
+                    }
+                }
+                else
+                {
+                    while (titleWindow.IndexOf(title) == -1 && titleWindow.IndexOf(title2) == -1)
+                    {
+                        time--;
+                        if (time <= 0)
+                            return null;
+
+                        Thread.Sleep(200);
+                        currentWindow = GetActiveWindowTitle();
+                        if (!string.IsNullOrEmpty(currentWindow.text))
+                        {
+                            titleWindow = ConvertToUnSign3(currentWindow.text).ToLower();
+                        }
+
+                    }
+                }
+                Thread.Sleep(100);
+                return currentWindow;
+            }
+            catch (Exception ex)
+            {
+                // Get stack trace for the exception with source file information
+                var st = new StackTrace(ex, true);
+                // Get the top stack frame
+                var frame = st.GetFrame(0);
+                // Get the line number from the stack frame
+                var line = frame.GetFileLineNumber();
+                APIManager.OpenNotePad(ex.Message + '\n' + "loi Line WebViewModel " + line + " Number Line " + APIManager.GetLineNumber(ex), "loi ");
+                throw;
+            }
+
+        }
+
         [DllImport("user32")]
         [return: MarshalAs(UnmanagedType.Bool)]
         private static extern bool EnumChildWindows(IntPtr window, EnumWindowProc callback, IntPtr lParam);
 
+        [DllImport("user32.dll", EntryPoint = "FindWindowEx")]
+        private static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+
+        [DllImport("user32.dll")]
+        static extern bool PostMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
+
+        [DllImport("User32.dll", EntryPoint = "SendMessage")]
+        private static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, string lParam);
+
+        [DllImport("winspool.drv", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool SetDefaultPrinter(string Name);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowText")]
+        private static extern int SetWindowText(IntPtr hWnd, string text);
+
+        public delegate bool EnumWindowProc(IntPtr hwnd, IntPtr lParam);
+        public static string namePrinterBD10 = "";
+        public static string namePrinterBD8 = "";
         private const int WM_CLOSE = 0x0010;
+        const int WM_COMMAND = 0x0111;
         private const int WM_GETTEXT = 0x000D;
         private const int WM_GETTEXTLENGTH = 0x000E;
     }

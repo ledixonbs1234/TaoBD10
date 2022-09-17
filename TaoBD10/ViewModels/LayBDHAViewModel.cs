d01fec8f0f645fffcaf3a688cd7fa42ea922b440
ledixonbs1234 <ledixonbs12345@gmail.com>
2022-09-17 20:52:20 +0700

    dd

--- TaoBD10/ViewModels/LayBDHAViewModel.cs
+++ TaoBD10/ViewModels/LayBDHAViewModel.cs
@@ -140,6 +140,7 @@ namespace TaoBD10.ViewModels
             string lastText = "";
             int countSame = 0;
             List<BD10DenInfo> bD10Dens = new List<BD10DenInfo>();
+            string test ="";
             while (countSame <= 3)
             {
                 string textClip = APIManager.GetCopyData();
@@ -161,6 +162,8 @@ namespace TaoBD10.ViewModels
                     List<string> listString = textClip.Split('\t').ToList();
                     if (listString.Count >= 6)
                     {
+                        test += listString[0] + '\n';
+                        
                         bD10Dens.Add(new BD10DenInfo(APIManager.ConvertToUnSign3(listString[0]), listString[2], listString[4], listString[5], APIManager.ConvertToUnSign3(listString[6])));
                     }
                     else
@@ -175,7 +178,7 @@ namespace TaoBD10.ViewModels
             }
             if (bD10Dens.Count > 0)
             {
-                APIManager.ShowSnackbar(bD10Dens[0].Name);
+                APIManager.OpenNotePad(test, "df");
                 string jsonText = JsonConvert.SerializeObject(bD10Dens, Formatting.None);
                 MqttManager.Pulish(FileManager.MQTTKEY + "_laydanhsachbd", jsonText);
 

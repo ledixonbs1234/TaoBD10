0e0fbd206c1a14260dc5c651630232ae83b5073d
ledixonbs1234 <ledixonbs12345@gmail.com>
2022-10-03 18:09:42 +0700

    delete sound ok

--- TaoBD10/ViewModels/MainViewModel.cs
+++ TaoBD10/ViewModels/MainViewModel.cs
@@ -1610,7 +1610,7 @@ namespace TaoBD10.ViewModels
                 if (MessageQueue == null)
                     MessageQueue = new SnackbarMessageQueue();
                 MessageQueue.Enqueue(content, null, null, null, false, false, TimeSpan.FromSeconds(2));
-                SoundManager.playSound2(@"Number\tingting.wav");
+                //SoundManager.playSound2(@"Number\tingting.wav");
             });
         }
 

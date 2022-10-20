eb0111b53faa2b89086fb844e07e7be3eed20953
ledixonbs1234 <ledixonbs12345@gmail.com>
2022-10-20 22:25:17 +0700

    sdsd

--- TaoBD10/Views/WebView.xaml.cs
+++ TaoBD10/Views/WebView.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.Messaging;
+using System;
+using System.Drawing;
+using System.IO;
 using System.Windows.Controls;
 using TaoBD10.Model;
 
@@ -12,7 +15,16 @@ namespace TaoBD10.Views
         public WebView()
         {
             InitializeComponent();
+            //Browser.Paint += Browser_Paint;
         }
+
+        //private void Browser_Paint(object sender, CefSharp.Wpf.PaintEventArgs e)
+        //{
+        //    Bitmap newBitmap = new Bitmap(e.Width, e.Height, 4 * e.Width, System.Drawing.Imaging.PixelFormat.Format32bppRgb, e.Buffer);
+
+        //    var aPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "TestImageCefSharpQuant.png");
+        //    newBitmap.Save(aPath);
+        //}
 
         private void Browser_IsBrowserInitializedChanged(object sender, System.Windows.DependencyPropertyChangedEventArgs e)
         {

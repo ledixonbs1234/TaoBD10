0bc049baf762f2613922484ea2a3b6d473864284
ledixonbs1234 <ledixonbs12345@gmail.com>
2022-10-23 21:17:12 +0700

    dfdf

--- TaoBD10/ViewModels/ChiTietViewModel.cs
+++ TaoBD10/ViewModels/ChiTietViewModel.cs
@@ -92,11 +92,13 @@ namespace TaoBD10.ViewModels
                         if(haveItem != null)
                         {
                             haveItem.IsTamQuan = "TamQuan";
+                            haveItem.TrangThaiBD = TrangThaiBD.TamQuan;
                         }
                         var haveItem1 = LocKhaiThac.HangHoas.FirstOrDefault(a => maHangHoa.TuiHangHoa.SHTui.ToUpper() == a.TuiHangHoa.SHTui.ToUpper());
                         if(haveItem1 != null)
                         {
                             haveItem1.IsTamQuan = "TamQuan";
+                            haveItem.TrangThaiBD = TrangThaiBD.TamQuan;
                         }
                     }
 

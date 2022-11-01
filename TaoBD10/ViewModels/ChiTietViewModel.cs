d60502d3244897f520b59edd596ecd76045697c7
ledixonbs1234 <ledixonbs12345@gmail.com>
2022-11-01 21:16:48 +0700

    fix

--- TaoBD10/ViewModels/ChiTietViewModel.cs
+++ TaoBD10/ViewModels/ChiTietViewModel.cs
@@ -919,16 +919,20 @@ namespace TaoBD10.ViewModels
             {
                 foreach (HangHoaDetailModel hangHoa in ListShowHangHoa)
                 {
-                    if (hangHoa.BuuCucChapNhan.IndexOf("593200") != -1 && hangHoa.TuiHangHoa.KhoiLuong == "1,0" || hangHoa.TuiHangHoa.KhoiLuong == "1,5" || hangHoa.TuiHangHoa.KhoiLuong == "2,0" || hangHoa.TuiHangHoa.KhoiLuong == "2,5")
-                        hangHoa.TrangThaiBD = TrangThaiBD.KhongIn;
+                    if (!string.IsNullOrEmpty(hangHoa.BuuCucChapNhan))
+                        if (hangHoa.BuuCucChapNhan.IndexOf("593200") != -1 && hangHoa.TuiHangHoa.KhoiLuong == "1,0" || hangHoa.TuiHangHoa.KhoiLuong == "1,5" || hangHoa.TuiHangHoa.KhoiLuong == "2,0" || hangHoa.TuiHangHoa.KhoiLuong == "2,5")
+                            hangHoa.TrangThaiBD = TrangThaiBD.KhongIn;
                 }
             }
             else
             {
                 foreach (HangHoaDetailModel hangHoa in ListShowHangHoa)
                 {
-                    if (hangHoa.BuuCucChapNhan.IndexOf("593200") != -1 && hangHoa.TuiHangHoa.KhoiLuong == "1,0" || hangHoa.TuiHangHoa.KhoiLuong == "1,5" || hangHoa.TuiHangHoa.KhoiLuong == "2,0" || hangHoa.TuiHangHoa.KhoiLuong == "2,5")
-                        hangHoa.TrangThaiBD = TrangThaiBD.ChuaChon;
+                    if (!string.IsNullOrEmpty(hangHoa.BuuCucChapNhan))
+                    {
+                        if (hangHoa.BuuCucChapNhan.IndexOf("593200") != -1 && hangHoa.TuiHangHoa.KhoiLuong == "1,0" || hangHoa.TuiHangHoa.KhoiLuong == "1,5" || hangHoa.TuiHangHoa.KhoiLuong == "2,0" || hangHoa.TuiHangHoa.KhoiLuong == "2,5")
+                            hangHoa.TrangThaiBD = TrangThaiBD.ChuaChon;
+                    }
                 }
             }
         }
